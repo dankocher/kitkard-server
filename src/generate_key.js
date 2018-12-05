7cb7a346d8cf39402087cc479cbfc1706da9ed9c
Daniel Chernyshev <droque123@gmail.com>
2018-12-05 14:47:30 +0300

    remove gitignore files

--- src/generate_key.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const generate_key = function() {
-    var sha = crypto.createHash('sha256');
-    sha.update(Math.random().toString());
-    return sha.digest('hex');
-};
-
-module.exports = generate_key;
